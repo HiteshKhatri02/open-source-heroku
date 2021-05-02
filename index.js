e298f15cf1f11ba4fa03767a6d235b3581abcdfa
hitesh_khatri_mtx <Hitesh.khatri@mtxb2b.com>
2021-05-02 11:11:05 +0530

    sa

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require("express");
 var app = express();
 var path = require("path");
-//const accQueries = require("./data/accountQueries");
+const accQueries = require("./accountQueries");
 const port = process.env.PORT || 5000;
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "./data")));
-//app.get('/getAccounts', accQueries.getAccounts);
+app.get('/getAccounts', accQueries.getAccounts);
 
 
 app.get('/', function(req, res) {
