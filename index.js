1714db92269825175f05c5f0a9b98895ec0c6ecb
hitesh_khatri_mtx <Hitesh.khatri@mtxb2b.com>
2021-05-02 09:50:49 +0530

    pushing to git

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const port = process.env.PORT || 4000;
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, './public')));
-app.post('/getAccounts', accQueries.createEmployee);
+app.use(express.static(path.join(__dirname, './data')));
+app.get('/getAccounts', accQueries.getAccounts);
 
 
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname, './public/form.html'));
+    res.sendFile(path.join(__dirname, './data/intro.html'));
+	res.send('you are one');
 });
 
 const server = app.listen(process.env.PORT || 5000, () => {
