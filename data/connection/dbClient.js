e199c8e4a63c2b74f438f017f3d2c7a87d308c11
hitesh_khatri_mtx <Hitesh.khatri@mtxb2b.com>
2021-05-02 10:58:59 +0530

    push

--- data/connection/dbClient.js
+++ data/connection/dbClient.js
@@ -9,9 +9,6 @@ var ret = unload.add(function(){
 ret.remove(); // removes the event-handler
 const client = new Client({
 	connectionString: process.env.DATABASE_URL,
-	ssl: {
-		rejectUnauthorized: false
-  },
   query_timeout: 2000, // number of milliseconds before a query call will timeout, default is no timeout
   connectionTimeoutMillis: 1000,
 });
