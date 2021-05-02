9f02946abe29871cb935a30165730dc75beccbc5
hitesh_khatri_mtx <Hitesh.khatri@mtxb2b.com>
2021-05-02 10:36:23 +0530

    messages

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-const accQueries = require('./accountQueries');
-const port = process.env.PORT || 4000;
+const accQueries = require('./data/accountQueries');
+const port = process.env.PORT || 5000;
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,5 +16,5 @@ app.get('/', function(req, res) {
 });
 
 const server = app.listen(process.env.PORT || 5000, () => {
-    console.log(`Express is working on port ${port}`);
+    console.log('Express is working on port ${port}');
 });
